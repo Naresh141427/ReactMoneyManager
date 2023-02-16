20fc6471339515a76340b7cf22787eda76f8f3e5
Naresh141427 <121325194+Naresh141427@users.noreply.github.com>
2023-02-16 23:51:30 +0530

    Update index.js

--- src/components/MoneyDetails/index.js
+++ src/components/MoneyDetails/index.js
@@ -1,15 +1,26 @@
 import './index.css'
 
 const MoneyDetails = props => {
-  const {transactionTypeList} = props
+  const {transactionTypeList, stateObject} = props
   const {optionId, displayText, imageUrl, className} = transactionTypeList
+  const {balance, income, expenses} = stateObject
+  let value
+  if (optionId === 'BALANCE') {
+    value = balance
+  } else if (optionId === 'INCOME') {
+    value = income
+  } else {
+    value = expenses
+  }
 
   return (
     <li className={className}>
       <img src={imageUrl} className="images" alt={className} />
       <div className="content-container">
-        <p className="title">Your {displayText}</p>
-        <p className="money">RS 0</p>
+        <p className="title" data-testid={`${className}Amount`}>
+          Your {displayText}
+        </p>
+        <p className="money">RS {value}</p>
       </div>
     </li>
   )

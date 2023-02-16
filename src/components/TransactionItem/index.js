cce139544fbb3a4d61c4e5340690a2539133b030
Naresh141427 <121325194+Naresh141427@users.noreply.github.com>
2023-02-16 23:52:25 +0530

    Update index.js

--- src/components/TransactionItem/index.js
+++ src/components/TransactionItem/index.js
@@ -1,18 +1,27 @@
 import './index.css'
 
-const TransactionItem = props => (
-  <li className="transaction-item">
-    <p className="title">Title</p>
-    <p className="amount">Amount</p>
-    <p className="type">Type</p>
-    <button className="delete-button" type="button">
-      <img
-        src="https://assets.ccbp.in/frontend/react-js/money-manager/delete.png"
-        alt="delete"
-        className="delete-image"
-      />
-    </button>
-  </li>
-)
+const TransactionItem = props => {
+  const {transactionDetails, onRemovingTransaction} = props
+  const {id, title, amount, type} = transactionDetails
+  const onDeletingItem = () => {
+    onRemovingTransaction(id, amount, type)
+  }
+
+  return (
+    <li className="transaction-item">
+      <p className="title">{title}</p>
+      <p className="amount">RS {amount}</p>
+      <p className="type">{type}</p>
+      <button className="delete-button" type="button" onClick={onDeletingItem}>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/delete.png"
+          alt="delete"
+          className="delete-image"
+          data-testid="delete"
+        />
+      </button>
+    </li>
+  )
+}
 
 export default TransactionItem
